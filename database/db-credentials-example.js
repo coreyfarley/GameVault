4a4deeece1baa8e25559f4ee5a5cf56eba98c7e4
Fox Caminiti <fox@foxcam.net>
2025-05-08 11:03:15 -0400

    add example credentials

---
+++ database/db-credentials-example.js
@@ -0,0 +1,10 @@
+// Copy to untracked file database/db-credentials.js before filling this out.
+
+const credentials = {
+    host: '',
+    user: '',
+    password: '',
+    database: '',
+};
+
+module.exports = credentials
